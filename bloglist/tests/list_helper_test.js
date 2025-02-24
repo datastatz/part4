c82a278ac6dd8708852fff048ede2af2884a93c0
scraper <155395474+datastatz@users.noreply.github.com>
2025-02-24 14:37:03 +0100

    Finished exercise 4.4

--- bloglist/tests/list_helper_test.js
+++ bloglist/tests/list_helper_test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test');
+const { test, describe } = require('node:test');
 const assert = require('node:assert');
 const listHelper = require('../utils/list_helper'); // Import our helper
 
@@ -8,3 +8,58 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1); // Expected output should be 1
 });
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      }
+    ];
+  
+    const multipleBlogs = [
+      {
+        _id: '1',
+        title: 'First Blog',
+        author: 'Author 1',
+        url: 'http://example.com/1',
+        likes: 10,
+        __v: 0
+      },
+      {
+        _id: '2',
+        title: 'Second Blog',
+        author: 'Author 2',
+        url: 'http://example.com/2',
+        likes: 20,
+        __v: 0
+      },
+      {
+        _id: '3',
+        title: 'Third Blog',
+        author: 'Author 3',
+        url: 'http://example.com/3',
+        likes: 30,
+        __v: 0
+      }
+    ];
+  
+    test('when list has only one blog, equals the likes of that blog', () => {
+      const result = listHelper.totalLikes(listWithOneBlog);
+      assert.strictEqual(result, 5);
+    });
+  
+    test('when list has multiple blogs, returns sum of likes', () => {
+      const result = listHelper.totalLikes(multipleBlogs);
+      assert.strictEqual(result, 60); // 10 + 20 + 30
+    });
+  
+    test('when list is empty, returns 0', () => {
+      const result = listHelper.totalLikes([]);
+      assert.strictEqual(result, 0);
+    });
+  });
--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,8 +1,13 @@
 const dummy = (blogs) => {
     return 1; // Always returns 1
   };
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0) // Sums all likes
+};
   
   module.exports = {
     dummy,
+    totalLikes,
   };
   
